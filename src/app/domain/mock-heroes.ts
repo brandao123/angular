08b1690c3b16e802f10aeeb4a38dd4ec40d90979
Gabriel <gabrielcandidobrandao@gmail.com>
2018-03-26 21:29:52 -0300

    Versao 2.0

---
+++ src/app/domain/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from "./hero";
+
+export const HEROES : Hero[] = [
+    {id: 11, name: 'Capitão América'},
+    {id: 12, name: 'Homem de Ferro'},
+    {id: 13, name: 'Homem Aranha'},
+    {id: 14, name: 'Thanos'},
+    {id: 15, name: 'Thor'},
+    {id: 16, name: 'Pantera Negra'},
+    {id: 17, name: 'Hulk'}
+]
